f722722dbda63d661c1eb1d9f02ced3489ab5180
Austin King <shout@ozten.com>
2010-11-05 14:33:07 -0700

    Script for IT during pushes

---
+++ bin/update_production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update script for staging server. Takes care of updating code repo, vendor
+# dir, and running DB migrations.
+
+HERE=`dirname $0`
+GIT=`which git`
+PYTHON=`which python2.6`
+
+pushd "$HERE/../" > /dev/null
+
+# pull actual code
+$GIT pull -q origin prod
+$GIT submodule update --init
+
+# pull vendor repo
+pushd vendor > /dev/null
+$GIT pull -q origin prod
+$GIT submodule update --init
+popd > /dev/null
+
+# Run database migrations.
+$PYTHON vendor/src/schematic/schematic migrations/
+
+# Minify assets.
+$PYTHON manage.py compress_assets
+
+$PYTHON manage.py update_product_details
+
+popd > /dev/null
