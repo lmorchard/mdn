2fbc3a12196fe771290bf5f0f27da738bef7d56f
groovecoder <luke.crouch@gmail.com>
2011-03-29 15:47:53 -0500

    6811908..886f616 from kuma - bug 638620 change demo caching from cache-machine to cache so we can invalidate cache ourselves during demo submit; move page size into a setting

--- apps/demos/models.py
+++ apps/demos/models.py
@@ -30,8 +30,6 @@ from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify, filesizeformat
 
-import caching.base
-
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 
@@ -224,7 +222,7 @@ class OverwritingImageField(models.ImageField):
     attr_class = OverwritingImageFieldFile
 
 
-class SubmissionManager(caching.base.CachingManager):
+class SubmissionManager(models.Manager):
     """Manager for Submission objects"""
 
     # TODO: Make these search functions into a mixin?
@@ -288,7 +286,7 @@ class SubmissionManager(caching.base.CachingManager):
             return queryset.order_by('-created')
         
 
-class Submission(caching.base.CachingMixin, models.Model):
+class Submission(models.Model):
     """Representation of a demo submission"""
     objects = SubmissionManager()
 
